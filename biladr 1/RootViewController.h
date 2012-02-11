20adb80a736dbcc4836126e9addc279ed36c7658
Lion User <lionuser@Lions-Mac-Pro.local>
2012-02-11 11:09:58 +0100

    Initial Commit

---
+++ biladr 1/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  biladr 1
+//
+//  Created by Lion User on 2/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ biladr 1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  biladr 1
+//
+//  Created by Lion User on 2/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
